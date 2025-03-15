2dc3100987f42fa461205a04644336fbfdc4aa61
Likhith Sai Charan Jupudi <143252814+Likhith-Sai-Charan@users.noreply.github.com>
2025-03-15 15:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 function convertToRoman(num) {
     const symbs = [
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1]
-    ];
+	    ["M", 1000],
+	    ["CM", 900],
+	    ["D", 500],
+	    ["CD", 400],
+	    ["C", 100],
+	    ["XC", 90],
+	    ["L", 50],
+	    ["XL", 40],
+	    ["X", 10],
+	    ["IX", 9],
+	    ["V", 5],
+	    ["IV", 4],
+	    ["I", 1]
+	];
+    
+	let roman = "";
 
-    let roman = "";
-
-    symbs.forEach(([symb, val]) => {
+    symbs.forEach((item) => {
+		const [symb, val]=item;
+		
         while (num >= val) {
             roman += symb;
             num -= val;
         }
-    });
+    })
 
     return roman;
 }
+
+console.log(convertToRoman(14));
+console.log(convertToRoman(36))
 
 // You can test your code by running the above function:
 console.log(convertToRoman(798)); // Output: DCCXCVIII
 
 // Do not edit below this line
-//module.exports = convertToRoman;
+module.exports = convertToRoman;
